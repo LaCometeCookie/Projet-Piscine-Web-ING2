dacd8c3f3355108b1b818b8880039fc5cc4cb89c
Penginsan <64909107+Penginsan168@users.noreply.github.com>
2024-06-02 17:12:57 +0200

    Bootstrap
    Bootstrap Parcourir

--- parcourir.php
+++ parcourir.php
@@ -1,8 +1,3 @@
-<!-- Fenêtre qui affiche en fonction du choix de l'utilisateur : 
- 1. Un menu pour effectuer son choix (par défaut)
- 2. La liste des médecins avec sélection possible
- 3. La liste des services avec sélection possible (depuis les laboratoires)
- 4. La liste des laboratoires avec sélection possible-->
 <!DOCTYPE html>  
 <head>  
 <title>Medicare | Parcourir</title>  
@@ -16,6 +11,37 @@
  
  <!-- Dernier JavaScript compilé --> 
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
+
+ <script>
+     $(document).ready(function() {
+         $('#choix').change(function() {
+             var selected = $(this).val();
+             $('.section').hide();
+             $('#' + selected).show();
+         });
+     });
+ </script>
+  <style>
+        .navbar-nav {
+            display: flex;
+            justify-content: center;
+            width: 100%;
+        }
+        .navbar-nav > li {
+            float: none;
+        }
+        .navbar-brand {
+            display: flex;
+            align-items: center;
+        }
+        .navbar-brand img {
+            max-height: 40px; /* A ajuster*/
+            margin-right: 10px;
+        }
+        .nav-item-parcourir a {
+            color: blue !important;
+        }
+    </style> 
 </head>  
 <body>
      <?php
@@ -50,198 +76,184 @@
           $ok = FALSE;
      }
 	
-	if($ok)
-	{
-          
-  	?><!-- Boutons communs à toutes les fenêtres (sauf connexion) -->
-       <button type="button" class="btn btn-link">
-            <a href = "index.php">Accueil</a></button> 
-       <button type="button" class="btn btn-link">
-            <a href = "parcourir.php">Parcourir</a>
-       </button>
-       <button type="button" class="btn btn-link">
-            <a href = "rdv.php">Rendez-vous</a>
-       </button>
-       <button type="button" class="btn btn-link">
-            <a href = "profil.php"><?php echo htmlspecialchars($donnees['Nom']) ." ". htmlspecialchars($donnees['Prenom']);?></a></button>
-       <button type="button" class="btn btn-info"> 
-            <a href = "recherche.php"><span class="glyphicon glyphicon-search"></span> Recherche </a>
-       </button> <?php
-	 }
-     else
-     {
-          ?>
-     <!-- Boutons communs à toutes les fenêtres (sauf connexion) -->
-     <button type="button" class="btn btn-link">
-          <a href = "index.php">Accueil</a></button> 
-     <button type="button" class="btn btn-link">
-          <a href = "parcourir.php">Parcourir</a>
-     </button>
-     <button type="button" class="btn btn-link">
-          <a href = "rdv.php">Rendez-vous</a>
-     </button>
-     <button type="button" class="btn btn-link">
-          <a href = "connexion.php">Compte</a></button>
-     <button type="button" class="btn btn-info"> 
-          <a href = "recherche.php"><span class="glyphicon glyphicon-search"></span> Recherche </a>
-     </button> 
-     <?php
-     }
-     ?>
-     <!--Objectif recherché : garder la fenêtre en focntion du choix de l'utilisateur (utilisation de style.display pour gérer les blocs devant apparaitre ou non)-->
-     <div class="parcours">
-          <select name="choix" id="choix">
-               <optgroup label="choix">
-               <option value="medecinsg" id='choix' onclick ="document.getElementById('medecinsg').style.display = 'block' ;
-               document.getElementById('medecinsp').style.display = 'none';
-               document.getElementById('labos').style.display = 'none' ;">Médecins généralistes</option>
-               <option value="medecinsp" id='choix' onclick ="document.getElementById('medecinsg').style.display = 'none' ;
-               document.getElementById('medecinsp').style.display = 'block' ;
-               document.getElementById('labos').style.display = 'none' ;">Médecins spécialistes</option>
-               <option value = "labos" id = 'choix' onclick ="document.getElementById('medecinsg').style.display = 'none' ;
-               document.getElementById('medecinsp').style.display = 'none' ;
-               document.getElementById('labos').style.display = 'block' ;">Laboratoires</option>
-               </optgroup>
-          </select>
-     </div>
-     <div id = "medecinsg" style="display: none"><!--Liste des médecins généralistes-->
-     <p>Les médecins généralistes</p>
-		<table class="centre">
-		<tr>
-			<th>Nom</th>
-			<th>Prenom</th>
-			<th>Spécialité</th>
-			<th>Mail</th>
-               <th>Telephone</th>
-               <th>CV</th>
-               <th>Photo</th>
-		</tr>
-          <?php //A bien agencer
-          $reponse = $bdd->query('SELECT Nom, Prenom, specialite, Mail, telephone, CV, photo FROM medecins WHERE specialite = "generaliste" ORDER BY Nom');
-          while ($donnees = $reponse->fetch())
-          {
-          ?>
-          
-               <tr>
-                    <td><?php  echo $donnees['Nom']; ?></td>
-                    <td><?php  echo $donnees['Prenom']; ?></td>
-                    <td><?php  echo $donnees['specialite']; ?></td>
-                    <td><?php  echo $donnees['Mail']; ?></td>
-                    <td>+33<?php  echo $donnees['telephone']; ?></td>
-                    <td><?php  echo $donnees['CV']; ?></td>
-                    <td><?php  echo $donnees['photo']; ?></td>
-                    <td><form method = "post" action = "rdv.php"><button type="button" class="btn btn-link">
-                    <a href = "rdv.php">Prendre un RDV</a></form></td>
-                    <td><form method = "post" action = "rdv.php"><button type="button" class="btn btn-link">
-                    <a href = "rdv.php">Contacter</a></form></td>
-               </tr>
+    ?>
 
-          <?php  
-          }
-          $reponse->closeCursor();
-          ?>
-          </table>
-     </div>
-     <div id = "medecinsp" style="display: none"><!--Liste des médecins spécialistes-->
-          <p>Les médecins spécialistes</p>
-          <table class="centre">
-		<tr>
-			<th>Nom</th>
-			<th>Prenom</th>
-			<th>Spécialité</th>
-			<th>Mail</th>
-               <th>Telephone</th>
-               <th>CV</th>
-               <th>Photo</th>
-		</tr>
-          <?php //A bien agencer
-          $reponse = $bdd->query('SELECT Nom, Prenom, specialite, Mail, telephone, CV, photo FROM medecins WHERE specialite != "generaliste" ORDER BY Nom');
-          while ($donnees = $reponse->fetch())
-          {
-          ?>
-          
-               <tr>
-                    <td><?php  echo $donnees['Nom']; ?></td>
-                    <td><?php  echo $donnees['Prenom']; ?></td>
-                    <td><?php  echo $donnees['specialite']; ?></td>
-                    <td><?php  echo $donnees['Mail']; ?></td>
-                    <td>+33<?php  echo $donnees['telephone']; ?></td>
-                    <td><?php  echo $donnees['CV']; ?></td>
-                    <td><?php  echo $donnees['photo']; ?></td>
-                    <td><form method = "post" action = "rdv.php"><button type="button" class="btn btn-link">
-                    <a href = "rdv.php">Prendre un RDV</a></form></td>
-                    <td><form method = "post" action = "rdv.php"><button type="button" class="btn btn-link">
-                    <a href = "rdv.php">Contacter</a></form></td>
-               </tr>
+    <!-- Navigation -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img src="logo.png" alt="Logo"> Medicare
+                </a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item-accueil"><a href="index.php">Accueil</a></li>
+                    <li class="nav-item-parcourir"><a href="parcourir.php">Parcourir</a></li>
+                    <li><a href="rdv.php">Rendez-vous</a></li>
+                    <li><a href="recherche.php"><span class="glyphicon glyphicon-search"></span> Recherche</a></li>
+					<?php if ($ok): ?>
+                        <li><a href="profil.php"><?php echo htmlspecialchars($donnees['Nom']) . " " . htmlspecialchars($donnees['Prenom']); ?></a></li>
+                    <?php else: ?>
+                        <li><a href="connexion.php">Compte</a></li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-          <?php  
-          }
-          $reponse->closeCursor();
-          ?>
-          </table>
+    <!--Objectif recherché : garder la fenêtre en fonction du choix de l'utilisateur (utilisation de style.display pour gérer les blocs devant apparaitre ou non)-->
+    <div class="parcours container">
+          <div class="form-group">
+               <label for="choix">Choix</label>
+               <select name="choix" id="choix" class="form-control">
+                    <option value="medecinsg">Médecins généralistes</option>
+                    <option value="medecinsp">Médecins spécialistes</option>
+                    <option value="labos">Laboratoires</option>
+               </select>
+          </div>
      </div>
-     <div id = "labos" style="display: none"><!--Affichage des informations du laboratoire puis de ses services en fonction du labo choisi-->
-          <div id = "labinfo" style="display: block">
-               <p>Les labos</p>
-               <table>
+     <div id="medecinsg" class="section" style="display: none"><!--Liste des médecins généralistes-->
+          <p>Les médecins généralistes</p>
+          <table class="table table-striped">
+               <thead>
                     <tr>
                          <th>Nom</th>
-                         <th>Adresse</th>
-                         <th>Salle</th>
+                         <th>Prenom</th>
+                         <th>Spécialité</th>
                          <th>Mail</th>
                          <th>Telephone</th>
+                         <th>CV</th>
+                         <th>Photo</th>
                     </tr>
-               <?php //A bien agencer
-               $reponse = $bdd->query('SELECT ID, Nom, Adresse, Salle, telephone, Mail FROM labos ORDER BY Nom');
-               while ($donnees = $reponse->fetch())
-               {
-               ?>
-               
+               </thead>
+               <tbody>
+                    <?php 
+                    $reponse = $bdd->query('SELECT Nom, Prenom, specialite, Mail, telephone, CV, photo FROM medecins WHERE specialite = "generaliste" ORDER BY Nom');
+                    while ($donnees = $reponse->fetch())
+                    {
+                    ?>
                     <tr>
-                         <td><?php  echo $donnees['Nom']; ?></td>
-                         <td><?php  echo $donnees['Adresse']; ?></td>
-                         <td><?php  echo $donnees['Salle']; ?></td>
-                         <td><?php  echo $donnees['Mail']; ?></td>
-                         <td>+33<?php  echo $donnees['telephone']; ?></td>
-                         <td><form method = "post" action = "#"><button type="button" class="btn btn-link">
-                         <a href = "#" onclick="document.getElementById('service').style.display = 'block';
-                         document.getElementById('labinfo').style.display = 'none'"><input type = "text" name = "ID" value = "<?php  echo $donnees['ID']; ?>" hidden>Les services</a>
-                         </button></form></td>
-                         <!--Infos services à gérer (apparition/disparition)-->
+                         <td><?php echo htmlspecialchars($donnees['Nom']); ?></td>
+                         <td><?php echo htmlspecialchars($donnees['Prenom']); ?></td>
+                         <td><?php echo htmlspecialchars($donnees['specialite']); ?></td>
+                         <td><?php echo htmlspecialchars($donnees['Mail']); ?></td>
+                         <td>+33<?php echo htmlspecialchars($donnees['telephone']); ?></td>
+                         <td><?php echo htmlspecialchars($donnees['CV']); ?></td>
+                         <td><?php echo htmlspecialchars($donnees['photo']); ?></td>
+                         <td><a href="rdv.php" class="btn btn-link">Prendre un RDV</a></td>
+                         <td><a href="rdv.php" class="btn btn-link">Contacter</a></td>
                     </tr>
-                    
                     <?php  
-               }
-               $reponse->closeCursor();
-               ?>
+                    }
+                    $reponse->closeCursor();
+                    ?>
+               </tbody>
+          </table>
+     </div>
+     <div id="medecinsp" class="section" style="display: none"><!--Liste des médecins spécialistes-->
+          <p>Les médecins spécialistes</p>
+          <table class="table table-striped">
+               <thead>
+                    <tr>
+                         <th>Nom</th>
+                         <th>Prenom</th>
+                         <th>Spécialité</th>
+                         <th>Mail</th>
+                         <th>Telephone</th>
+                         <th>CV</th>
+                         <th>Photo</th>
+                    </tr>
+               </thead>
+               <tbody>
+                    <?php 
+                    $reponse = $bdd->query('SELECT Nom, Prenom, specialite, Mail, telephone, CV, photo FROM medecins WHERE specialite != "generaliste" ORDER BY Nom');
+                    while ($donnees = $reponse->fetch())
+                    {
+                    ?>
+                    <tr>
+                         <td><?php echo htmlspecialchars($donnees['Nom']); ?></td>
+                         <td><?php echo htmlspecialchars($donnees['Prenom']); ?></td>
+                         <td><?php echo htmlspecialchars($donnees['specialite']); ?></td>
+                         <td><?php echo htmlspecialchars($donnees['Mail']); ?></td>
+                         <td>+33<?php echo htmlspecialchars($donnees['telephone']); ?></td>
+                         <td><?php echo htmlspecialchars($donnees['CV']); ?></td>
+                         <td><?php echo htmlspecialchars($donnees['photo']); ?></td>
+                         <td><a href="rdv.php" class="btn btn-link">Prendre un RDV</a></td>
+                         <td><a href="rdv.php" class="btn btn-link">Contacter</a></td>
+                    </tr>
+                    <?php  
+                    }
+                    $reponse->closeCursor();
+                    ?>
+               </tbody>
+          </table>
+     </div>
+     <div id="labos" class="section" style="display: none"><!--Affichage des informations du laboratoire puis de ses services en fonction du labo choisi-->
+          <div id="labinfo" style="display: block">
+               <p>Les labos</p>
+               <table class="table table-striped">
+                    <thead>
+                         <tr>
+                              <th>Nom</th>
+                              <th>Adresse</th>
+                              <th>Salle</th>
+                              <th>Mail</th>
+                              <th>Telephone</th>
+                         </tr>
+                    </thead>
+                    <tbody>
+                         <?php 
+                         $reponse = $bdd->query('SELECT ID, Nom, Adresse, Salle, telephone, Mail FROM labos ORDER BY Nom');
+                         while ($donnees = $reponse->fetch())
+                         {
+                         ?>
+                         <tr>
+                              <td><?php echo htmlspecialchars($donnees['Nom']); ?></td>
+                              <td><?php echo htmlspecialchars($donnees['Adresse']); ?></td>
+                              <td><?php echo htmlspecialchars($donnees['Salle']); ?></td>
+                              <td><?php echo htmlspecialchars($donnees['Mail']); ?></td>
+                              <td>+33<?php echo htmlspecialchars($donnees['telephone']); ?></td>
+                              <td><a href="#" class="btn btn-link" onclick="document.getElementById('service').style.display = 'block'; document.getElementById('labinfo').style.display = 'none';"><input type="text" name="ID" value="<?php echo htmlspecialchars($donnees['ID']); ?>" hidden>Les services</a></td>
+                         </tr>
+                         <?php  
+                         }
+                         $reponse->closeCursor();
+                         ?>
+                    </tbody>
                </table>
           </div>
-          <div id = "service" style = "display:none">
-          <p>Les labos</p>
-               <table>
-               <?php //A bien agencer
-               $ID = (int)$_POST['ID'];
-               $reponse = $bdd->query("SELECT Service1, Service2, Service3 FROM labos WHERE ID = $ID");
-               
-               $donnees = $reponse->fetch();
-               ?>
-                    <tr>
-                         <td><?php  echo htmlspecialchars($donnees['Service1']); ?></td>
-                         <td><?php  echo htmlspecialchars($donnees['Service2']); ?></td>
-                         <td><?php  echo htmlspecialchars($donnees['Service3']); ?></td>
-                         <td><button type="button" class="btn btn-link">
-                         <a href = "#" onclick="document.getElementById('service').style.display = 'none';
-                         document.getElementById('labinfo').style.display = 'block'">Les labos</a></button></td>
-                         <!--Infos services à gérer (apparition/disparition)-->
-                    </tr>       
-                    <?php  
-               
-               $reponse->closeCursor();
-               ?>
+          <div id="service" style="display:none">
+               <p>Les services</p>
+               <table class="table table-striped">
+                    <tbody>
+                         <?php 
+                         $ID = (int)$_POST['ID'];
+                         $reponse = $bdd->prepare("SELECT Service1, Service2, Service3 FROM labos WHERE ID = :ID");
+                         $reponse->bindParam(':ID', $ID, PDO::PARAM_INT);
+                         $reponse->execute();
+                         $donnees = $reponse->fetch();
+                         ?>
+                         <tr>
+                              <td><?php echo htmlspecialchars($donnees['Service1']); ?></td>
+                              <td><?php echo htmlspecialchars($donnees['Service2']); ?></td>
+                              <td><?php echo htmlspecialchars($donnees['Service3']); ?></td>
+                              <td><a href="#" class="btn btn-link" onclick="document.getElementById('service').style.display = 'none'; document.getElementById('labinfo').style.display = 'block';">Les labos</a></td>
+                         </tr>
+                         <?php  
+                         $reponse->closeCursor();
+                         ?>
+                    </tbody>
+               </table>
           </div>
      </div>
      <?php
           mysqli_close($db_handle); 
-          ?>
+     ?>
+     <!-- Footer -->
+    <footer class="text-center mt-4">
+        <p>&copy; 2024 Medicare. Tous droits réservés.</p>
+        <p>Adresse: 1234 Rue de la Santé, 75000 Paris, France</p>
+    </footer>
 </body>  
-</html> 
+</html>
