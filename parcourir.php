96b1ce3af54fbb33a62b857abd7de9166139c21e
Penginsan <64909107+Penginsan168@users.noreply.github.com>
2024-06-02 20:51:54 +0200

    Update parcourir.php
    Navigation bar

--- parcourir.php
+++ parcourir.php
@@ -96,13 +96,13 @@
                    <ul class="nav navbar-nav">
                        <li class="nav-item-accueil"><a href="index.php">Accueil</a></li>
                        <li class="nav-item-parcourir"><a href="parcourir.php">Parcourir</a></li>
-                       <li class="nav-item-rdv"><a href="rdv.php">Rendez-vous</a></li>
-                       <li class="nav-item-recherche"><a href="recherche.php"><span class="glyphicon glyphicon-search"></span> Recherche</a></li>
-                            <?php if ($ok): ?>
-                           <li class="nav-item-compte"><a href="profil.php"><?php echo htmlspecialchars($donnees['Nom']) . " " . htmlspecialchars($donnees['Prenom']); ?></a></li>
+                       <li class="nav-item-rdv"><a href="rdv.php">Rendez-vous</a></li>                
+                       <?php if ($ok): ?>
+                       <li class="nav-item-compte"><a href="profil.php"><?php echo htmlspecialchars($donnees['Nom']) . " " . htmlspecialchars($donnees['Prenom']); ?></a></li>
                        <?php else: ?>
-                           <li><a href="connexion.php">Compte</a></li>
+                       <li><a href="connexion.php">Compte</a></li>
                        <?php endif; ?>
+		       <li class="nav-item-recherche"><a href="recherche.php"><span class="glyphicon glyphicon-search"></span> Recherche</a></li>
                    </ul>
                </div>
           </div>
