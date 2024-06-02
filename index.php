ad88e0d20104f9e4dc00a9fd6d506c8fb8be40f9
LaCometeCookie <come.boutevin@gmail.com>
2024-06-02 21:58:42 +0200

    Caroussel

--- index.php
+++ index.php
@@ -182,6 +182,66 @@
           </div>
      </nav>
 
+
+    <!-- Caroussel -->
+    <div class="container">
+        <h2>Les capitales européennes</h2>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+            </ol>
+
+            <!-- Wrapper pour les images -->
+            <div class="carousel-inner">
+
+                <div class="item active">
+                    <img src="images/paris.jpeg" alt="Paris" style="width:100%; height: 800px;">
+                    <div class="carousel-caption">
+                        <h3>Paris</h3>
+                        <p>La ville lumière!</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="images/berlin.jpg" alt="Berlin" style="width:100%; height: 800px;">
+                    <div class="carousel-caption">
+                        <h3>Berlin</h3>
+                        <p>Tu te perds en vélo</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="images/prague.jpg" alt="Prague" style="width:100%; height: 800px;">
+                    <div class="carousel-caption">
+                        <h3>Prague</h3>
+                        <p>Tchéquie</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="images/rome.jpg" alt="Rome" style="width:100%; height: 800px;">
+                    <div class="carousel-caption">
+                        <h3>Rome</h3>
+                        <p>On ne coupe pas les en deux</p>
+                    </div>
+                </div>
+
+
+            </div>
+            <!-- Controles à gauche et à droite -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Précédent</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Suivant</span>
+            </a>
+        </div>
+    </div>
+
+
     <!-- Actualités -->
       <div class="container text-center">
           <br>
